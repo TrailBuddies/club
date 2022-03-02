6ee0c3a4787e5e6c5c97ebba5618bb99aa8e1954
matievisthekat <matievisthekat@gmail.com>
2022-03-02 21:20:36 +0200

    add postcss-sass

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-	plugins: [require('autoprefixer'), require('postcss-preset-env'), require('cssnano'), require('postcss-sass')]
+	plugins: [require('autoprefixer'), require('postcss-preset-env'), require('cssnano')]
 };
